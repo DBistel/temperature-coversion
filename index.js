04f6e4ce07f6da3cb9ec97aa92c7d66f6901e8e8
dbistel <ddbistel@gmail.com>
2025-06-06 17:04:19 -0600

    added prompt. used a parseFloat incase they enter the number before the degree and avoid a string. ajusted describeTemperature so that it reflects temperatures that are equle to each designation.

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ const Celsius = convertToCelsius(76)
 
 const describeTemperature = (Fahrenheit) => {
     let Celsius = convertToCelsius (Fahrenheit)
-    if (Celsius < 0){
+    if (Celsius <= 0){
         return `The current temperature is ${Fahrenheit}°F/ ${Celsius}°C, it is very cold`
-    } else if (Celsius < 20){
+    } else if (Celsius <= 20){
          return `The current temperature is ${Fahrenheit}°F/ ${Celsius}°C, it is cold`
-    } else if (Celsius < 30){
+    } else if (Celsius <= 30){
          return `The current temperature is ${Fahrenheit}°F/ ${Celsius}°C, it is warm`
-    } else if (Celsius < 40){
+    } else if (Celsius <= 40){
          return `The current temperature is ${Fahrenheit}°F/ ${Celsius}°C, it is hot`
     } else {
          return `The current temperature is ${Fahrenheit}°F/ ${Celsius}°C, it is very hot`
     }
 
 };
-let temperature = describeTemperature(110);
+//let temperature = describeTemperature(110);
 //console.log(temperature);
 
+let person = prompt("Please enter the temperature in Fahrenheit", "°F");
+//const Temp = parseFloat(person)
+const temperature = describeTemperature(parseFloat(person))
+//parseFloat can be nested into the describeTemperature function.
+console.log(temperature)
